dd192a91bafcedaedb276090ae1dffbebb652160
alpitg <31066944+alpitg@users.noreply.github.com>
2019-05-16 00:16:57 +0530

    pd upload on server and the path of image

--- src/app/pdf-sample/pdf-sample.component.ts
+++ src/app/pdf-sample/pdf-sample.component.ts
@@ -20,22 +20,44 @@ export class PdfSampleComponent implements OnInit {
   onExportPdf() {
     console.log('pdf');
     const doc = new jsPDF();
+//     const img = new Image();
 
     const pageHeight = doc.internal.pageSize.height;
     const pageWidth = doc.internal.pageSize.width;
 
-    doc.setFontSize(14);
-    doc.text(
-      'NEVADA STATE CONTRACTORS BOARD',
-      StaticUtilities.pdfTextCenter(
-        doc,
+//     const thisConst = this;
+    
+//     img.onload = function() {
+//       doc.addImage(this, 10, 10, 25, 25);
+      
+      doc.setFontSize(14);
+      doc.text(
         'NEVADA STATE CONTRACTORS BOARD',
-        14
-      ),
-      15
-    );
+        StaticUtilities.pdfTextCenter(
+          doc,
+          'NEVADA STATE CONTRACTORS BOARD',
+          14
+        ),
+        15
+      );
 
-    doc.save('test.pdf');
+      doc.save('test.pdf');
+    
+    //--------------- Use below code to upload on server ---------------//
+      // NOTE: BLOB to File conversion is must & filename.extension is compulsory
+      // const file = new File([doc.output('blob')], 'filename.pdf', {type: 'application/pdf', lastModified: Date.now()});
+      // console.log(file);
+
+      // const formData = new FormData(); // To carry on your data
+      // formData.append('file', file);
+      // formData.append('data', JSON.stringify(thisConst.signature));
+      // thisConst.uploadDocument(formData); // Upload
+    
+    
+//     };
+//     img.crossOrigin = '';
+//     img.src = '../../assets/images/nevida_state_pdf_logo.PNG';
+
   }
 
 
